51bfe2355e60d3d8103391d9603f7b564caaa793
Matheus Marsiglio <matmarsiglio@gmail.com>
2019-04-28 17:27:46 -0300

    feat(Logger): add colors to log levels

--- src/logger/logger.rs
+++ src/logger/logger.rs
@@ -3,15 +3,22 @@ extern crate chrono;
 extern crate fern;
 
 use std::io;
+use fern::colors::{Color, ColoredLevelConfig};
 
 pub fn setup_logger() -> Result<(), fern::InitError> {
+    let colors = ColoredLevelConfig::new()
+        .info(Color::Green)
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        .debug(Color::Magenta);
+
     fern::Dispatch::new()
-        .format(|out, message, record| {
+        .format(move |out, message, record| {
             out.finish(format_args!(
                 "{}[{}][{}] {}",
                 chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
                 record.target(),
-                record.level(),
+                colors.color(record.level()),
                 message
         ))
     })
